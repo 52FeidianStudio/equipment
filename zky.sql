8042882607373a4970f6fe6c56628cb56a57d1c3
doubibobo <15071267961@163.com>
2016-10-21 12:52:11 +0800

    sql update

--- zky.sql
+++ zky.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2016-10-18 17:51:08
+-- Generation Time: 2016-10-21 06:44:09
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `tp_answer` (
   `content` text NOT NULL,
   `date` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=53 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=56 ;
 
 --
 -- 转存表中的数据 `tp_answer`
@@ -74,7 +74,10 @@ INSERT INTO `tp_answer` (`id`, `userid`, `messageid`, `content`, `date`) VALUES
 (49, 6, 17, '瓦咔咔', '2016-10-18 21:16:19'),
 (50, 6, 17, '哈哈哈', '2016-10-18 21:17:47'),
 (51, 6, 17, '现在是北京时间21:21', '2016-10-18 21:21:33'),
-(52, 6, 39, '17日凌晨4时15分许，航天员出征仪式在酒泉卫星发射中心问天阁举行。范长龙代表党中央、国务院和中央军委，代表习近平总书记，亲切看望了执行神舟十一号载人飞行任务的航天员景海鹏、陈冬。\r\n\r\n航天员们郑重表示，服从命令，听从指挥，沉着冷静，精心操作，圆满完成任务，请祖国和人民放心。在送行人群的鼓掌欢呼声中航天员们乘车前往发射场，进入飞船作发射前的准备。', '2016-10-18 21:52:15');
+(52, 6, 39, '17日凌晨4时15分许，航天员出征仪式在酒泉卫星发射中心问天阁举行。范长龙代表党中央、国务院和中央军委，代表习近平总书记，亲切看望了执行神舟十一号载人飞行任务的航天员景海鹏、陈冬。\r\n\r\n航天员们郑重表示，服从命令，听从指挥，沉着冷静，精心操作，圆满完成任务，请祖国和人民放心。在送行人群的鼓掌欢呼声中航天员们乘车前往发射场，进入飞船作发射前的准备。', '2016-10-18 21:52:15'),
+(53, 6, 41, '666', '2016-10-19 13:07:23'),
+(54, 6, 1, 'wakakaka', '2016-10-19 17:57:59'),
+(55, 6, 39, '收到！', '2016-10-19 22:15:22');
 
 -- --------------------------------------------------------
 
@@ -98,16 +101,18 @@ CREATE TABLE IF NOT EXISTS `tp_equipment` (
   `eblong` varchar(32) NOT NULL,
   `imagicname` varchar(32) NOT NULL,
   `imagicaddress` varchar(100) NOT NULL,
+  `class` varchar(32) NOT NULL DEFAULT 'other',
+  `introduction` text,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=31 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=32 ;
 
 --
 -- 转存表中的数据 `tp_equipment`
 --
 
-INSERT INTO `tp_equipment` (`id`, `eid`, `ecname`, `eename`, `etype`, `ecmanufactor`, `eemanufactor`, `eoutdate`, `eoutid`, `ebuydate`, `eprice`, `elocation`, `eblong`, `imagicname`, `imagicaddress`) VALUES
-(30, 2, '刘晓磊', 'Liuxiaolei', '女', '山东省临沂市莒南县', 'Shandong Linyi', '1996-09-08', '1997', '12', '￥4444', '当发', '2016-10-13 10:57:45', '1476370665.jpg', './Equipment/2016-10-13/1476370665.jpg'),
-(29, 371327, '莒南三中', 'school 3', 'student', '山东', 'Shandong', '1956', '003', '1989', '￥80489494514', '十字路', '2016-10-13 10:48:41', '1476370121.jpeg', './Equipment/2016-10-13/1476370121.jpeg');
+INSERT INTO `tp_equipment` (`id`, `eid`, `ecname`, `eename`, `etype`, `ecmanufactor`, `eemanufactor`, `eoutdate`, `eoutid`, `ebuydate`, `eprice`, `elocation`, `eblong`, `imagicname`, `imagicaddress`, `class`, `introduction`) VALUES
+(30, 2, '刘晓磊', 'Liuxiaolei', '女', '山东省临沂市莒南县', 'Shandong Linyi', '1996-09-08', '1997', '12', '￥123', '逸夫楼信息学院办公室', '2016-10-13 10:57:45', '1476370665.jpg', './Equipment/2016-10-13/1476370665.jpg', 'audi', ''),
+(31, 371327, '朱传波', 'Doubibobo', '74LS00', '山东省临沂市莒南县', 'Linyi of Shandong prorvince', '1997-12-12', '371327199712121516', '2015-09-06', '$10000', '华中农业大学', '2016-10-20 23:20:10', '1476976810.jpg', './Equipment/2016-10-20/1476976810.jpg', 'volvo', NULL);
 
 -- --------------------------------------------------------
 
@@ -130,7 +135,8 @@ CREATE TABLE IF NOT EXISTS `tp_equipmentfile` (
 CREATE TABLE IF NOT EXISTS `tp_equipmentmanage` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `equipmentid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
+  `title` varchar(32) DEFAULT NULL,
+  `content` text,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -196,17 +202,14 @@ CREATE TABLE IF NOT EXISTS `tp_information` (
   `content` text NOT NULL,
   `title` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
 
 --
 -- 转存表中的数据 `tp_information`
 --
 
 INSERT INTO `tp_information` (`id`, `userid`, `date`, `content`, `title`) VALUES
-(6, 5, '2016-10-11 08:25:45', '同志们，为体现党对教育事业的关怀，根据总过经济建设发展的需要，特将国庆小长假由原来的七天延长至十五天，为充分体现对同志们的关怀，任何教师不准以任何形式布置任何作业', '关于国庆延长放假的通知'),
-(9, 1, '2016-10-11 08:34:23', '哈哈哈', '朱传波大帅哥'),
-(10, 1, '2016-10-15', '现在是北京时间10:30', '时间'),
-(11, 1, '2016-10-15', '沸点工作室是隶属我院的一些志同道合的，有能力的优秀人才聚集起来一起培养的优秀团队，为了吸收新鲜血液，定于下周开始沸点工作室的招新工作，各单位务必大力配合与支持', '关于加大对我院沸点工作室招新工作的支持');
+(15, 6, '2016-10-19', '习近平与贾大山\r\n\r\n　　习近平与作家贾大山的友情，同样朴素而动人。1998年，贾大山去世一周年时，习近平发表文章《忆大山》，历数了二人多年的情谊和对老友的怀念。习近平在文中饱含深情地写道，“大山的逝世，使我失去了一个好朋友、好兄长。我多么想亲自去为他送行，再看他最后一眼哪！”\r\n\r\n　　然而，当年习近平与贾大山初次见面时，气氛却并不算融洽。彼时，贾大山正在办公室里与几个文友讨论作品。他当过老师、编剧、导演和演员，博闻强记，口才极佳。那是一个文学的年代，到处是文学青年，到处是文学论坛。他的屋内，更是常常访客盈门。\r\n\r\n▲上世纪80年代贾大山（左一）参加人民文学笔会\r\n\r\n　　习近平穿着一件褪色的绿军装，像一名普通的退伍兵，又像一个青涩的文学青年。或许正是因此，当习近平进来的时候，谈兴正浓的贾大山没有注意到他，继续着自己的演说。等了一会儿，趁喝水的间歇，旁人上前介绍。贾大山才明白，面前这位高高大大、清清瘦瘦的青年，就是新来的县委副书记。\r\n\r\n　　接下来的场景曾让习近平印象深刻，贾大山扭头一转就说：“来了个嘴上没毛的管我们！”\r\n\r\n　　但是，习近平并没有介意，依然笑容满面。\r\n\r\n　　作家贾大山总是关注现实，解剖现实。他得奖的《取经》《花市》等作品，就是以政治视角描写基层干部和普通农民。对这座县城，这个国家，这个民族，他有着深深的热爱和关注。习近平与贾大山很快就谈到了一起，房间内的气氛逐渐活跃了起来。\r\n\r\n　　习近平在《忆大山》一文中记录了当时的情景：“虽然第一次见面，但我们却像多年不见的朋友，有说不完的话题，表不尽的情谊。临别时……我劝他留步，他像没听见似的。就这样边走边说，竟一直把我送到机关门口。”\r\n\r\n　　以后的日子里，每隔一段时间两人就要约见一次。有时是在习近平办公室，多数是在贾大山家里。', '习近平与他的作家“朋友圈”');
 
 -- --------------------------------------------------------
 
@@ -221,7 +224,7 @@ CREATE TABLE IF NOT EXISTS `tp_message` (
   `date` varchar(32) NOT NULL,
   `title` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=41 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=43 ;
 
 --
 -- 转存表中的数据 `tp_message`
@@ -243,7 +246,33 @@ INSERT INTO `tp_message` (`id`, `userid`, `content`, `date`, `title`) VALUES
 (15, 11, '今天是2015年10月17日下午16:19分', '1476692372', '四教'),
 (16, 11, '朱传波', '1476692474', '程天赐'),
 (17, 11, '他是程天赐', '2016-10-17 16:23:36', '我是朱传波'),
-(39, 6, '习近平指出，天宫二号和神舟十一号载人飞行任务，将首次实现我国航天员中期在轨驻留，开展一批体现国际科学前沿和高新技术发展方向的空间科学与应用任务，标志着我国载人航天工程取得了新的重大进展。\r\n\r\n习近平强调，太空探索永无止境，航天攻关任重道远。希望同志们大力弘扬载人航天精神，精心做好后续各项工作，确保实现既定任务目标，不断开创载人航天事业发展新局面，使中国人探索太空的脚步迈得更大更远，为建设航天强国作出新的贡献。', '2016-10-18 21-49-11', '习近平致电表示热烈祝贺神舟十一号载人飞船发射成功');
+(39, 6, '习近平指出，天宫二号和神舟十一号载人飞行任务，将首次实现我国航天员中期在轨驻留，开展一批体现国际科学前沿和高新技术发展方向的空间科学与应用任务，标志着我国载人航天工程取得了新的重大进展。\r\n\r\n习近平强调，太空探索永无止境，航天攻关任重道远。希望同志们大力弘扬载人航天精神，精心做好后续各项工作，确保实现既定任务目标，不断开创载人航天事业发展新局面，使中国人探索太空的脚步迈得更大更远，为建设航天强国作出新的贡献。', '2016-10-18 21-49-11', '习近平致电表示热烈祝贺神舟十一号载人飞船发射成功'),
+(41, 6, 'hahaha', '2016-10-19 13-07-16', 'hahaha'),
+(42, 6, '拉屎', '2016-10-19 22:17:37', '凯撒');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tp_regulation`
+--
+
+CREATE TABLE IF NOT EXISTS `tp_regulation` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `userid` int(32) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `date` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `realname` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 转存表中的数据 `tp_regulation`
+--
+
+INSERT INTO `tp_regulation` (`id`, `userid`, `address`, `date`, `name`, `realname`) VALUES
+(2, 6, './Regulation/2016-10-19/1476870670.docx', '2016-10-19 17:51:10', '', '展板 - 网站.docx'),
+(3, 6, './Regulation/2016-10-19/1476870700.zip', '2016-10-19 17:51:40', '', 'Emoji2.zip');
 
 -- --------------------------------------------------------
 
@@ -279,11 +308,11 @@ CREATE TABLE IF NOT EXISTS `tp_user` (
 --
 
 INSERT INTO `tp_user` (`id`, `username`, `password`, `isTeacher`, `name`, `say`, `time`) VALUES
-(9, '2015317200401', '852', 0, '项健健', 0, '2016-10-18 11:46:37'),
-(8, '2015317200426', '741', 0, '田尧', 0, '2016-10-18 11:46:37'),
-(7, '2015317200416', '789', 0, '占鹏', 0, '2016-10-18 11:46:37'),
+(9, '2015317200401', '852', 0, '项健健', 0, '2016-10-21 12:30:00'),
+(8, '2015317200426', '741', 0, '田尧', 0, '2016-10-21 12:30:00'),
+(7, '2015317200416', '789', 0, '占鹏', 0, '2016-10-21 12:30:00'),
 (6, '2015317200402', '123', 1, '朱传波', 0, ''),
-(11, '2015317200409', '123', 0, '黄海明', 0, '2016-10-18 11:46:37');
+(11, '2015317200409', '123', 0, '黄海明', 0, '2016-10-21 12:30:00');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
